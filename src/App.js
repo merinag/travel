bcebc3d031bdee8512ba21100b03ee67bce286ac
Eyobyb <eyobyirgu@gmail.com>
2023-12-07 00:03:50 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./component/home";
+import About from "./component/About";
+import Contact from "./component/contact";
+import Adventures from "./component/adventures";
+import Register from "./component/RegisterPage";
+import Login from "./component/LoginPage";
+import Booking from "./component/Book";
+import Add from "./component/add";
+import axios  from "axios";
 
 import Home from './component/home';
 import About from './component/About';
@@ -13,18 +23,41 @@ import Booking from './component/Book';
 import Add from './component/add';
 import Bookings from './component/Bookings';
 import Aaa from './component/Aaa';
+
 function App() {
+  const [loginoggedIn, setLoginoggedIn] = useState(false);
+  const runner = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/user", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      if (response.status == 200) {
+        setLoginoggedIn(true)
+      }
+    } catch (e) {
+      setLoginoggedIn(false)
+    }
+  
+  };
+  useEffect(()=>{
+},[loginoggedIn])
+  useEffect(() => {
+    runner();
+  }, []);
   return (
     <>
       <div className="body">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={loginoggedIn ? <About /> : <Login />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/adventures" element={<Adventures />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/book" element={<Booking />} />
+          <Route path="/book" element={loginoggedIn ? <Booking /> : <Login />} />
           <Route path="/add" element={<Add />} />
           <Route path="/aa" element={<Aaa />} />
           {/* <Route path="/bb" element={<Bookings />} /> */}
@@ -35,7 +68,3 @@ function App() {
 }
 
 export default App;
-
-//1, date
-// 2,quantity
-// 3,
